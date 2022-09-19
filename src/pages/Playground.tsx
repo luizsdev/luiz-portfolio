37f9028e3a4124c8a3350038a6c1378e4bce460b
luizsdev <luizsdev@gmail.com>
2022-09-19 01:49:33 -0300

    getting playground to work

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,9 +1,20 @@
 import '../styles/playground.css'
-import * as axios from 'axios'
+import axios from 'axios'
 import { useState } from 'react'
 export const Playground = () => {
   const [url, setUrl] = useState('')
-  const [method, setMethod] = useState('')
+  const [method, setMethod] = useState('GET')
+  function sendRequest(e: any) {
+    switch (method) {
+      case 'GET':
+        axios
+          .get(url)
+          .then((response) => {
+            console.log(response)
+          })
+          .catch((err) => console.log(err))
+    }
+  }
   function getUrl(e: any) {
     setUrl(e.target.value)
     console.log(url)
@@ -13,9 +24,24 @@ export const Playground = () => {
       <div className="playground rounded-lg">
         <input
           type="text"
-          className="url form-control input"
+          className="url  input input-bordered input-primary "
           onChange={getUrl}
         />
+        <button className="btn" onClick={sendRequest}>
+          SEND
+        </button>
+        <div className="method">
+          <select className="methods select w-full max-w-xs">
+            <option disabled selected>
+              SELECT THE METHOD
+            </option>
+            <option value="get">GET</option>
+            <option value="post">POST</option>
+            <option value="update">UPDATE</option>
+            <option value="delte">DELETE</option>
+          </select>
+        </div>
+        <div className="body rounded-lg"></div>
       </div>
     </div>
   )
